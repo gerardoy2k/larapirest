66bd5cd46266eb1a4df4a16d6ed960874ecde6fe
gerardoy2k <gerardoy2k@gmail.com>
2018-04-10 22:14:15 -0400

    ajustes migrations

--- database/migrations/2018_04_10_035746_create_rates_table.php
+++ database/migrations/2018_04_10_035746_create_rates_table.php
@@ -24,12 +24,6 @@ class CreateRatesTable extends Migration
             $table->foreign('modelo_id')->references('id')->on('modelos');
             $table->foreign('service_id')->references('id')->on('services');
         });
-        Schema::table('rates', function (Blueprint $table) {
-            $table->integer('modelo_id')->unsigned()->change();
-            $table->foreign('modelo_id')->references('id')->on('modelos')->change();
-            $table->integer('service_id')->unsigned()->change();
-            $table->foreign('service_id')->references('id')->on('services')->change();
-        });
     }
 
     /**
